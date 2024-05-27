18804dd8a7b364e7bfc96ee7cc762f6d0a05dd6e
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-05-27 09:32:35 +0200

    feat: updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,17 +8,15 @@ const config = withModuleFederationPlugin({
     './OneCXParameterModule': 'src/app/onecx-parameter-remote.module.ts'
   },
   shared: share({
-    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/core': { singleton: true, requiredVersion: 'auto' },
     '@angular/forms': {
       singleton: true,
-      strictVersion: true,
       requiredVersion: 'auto',
       includeSecondaries: true,
       eager: false
     },
     '@angular/common': {
       singleton: true,
-      strictVersion: true,
       requiredVersion: 'auto',
       includeSecondaries: {
         skip: ['@angular/common/http/testing']
@@ -26,13 +24,12 @@ const config = withModuleFederationPlugin({
     },
     '@angular/common/http': {
       singleton: true,
-      strictVersion: true,
       requiredVersion: 'auto',
       includeSecondaries: true
     },
-    rxjs: { singleton: true, strictVersion: true, requiredVersion: 'auto', includeSecondaries: true },
-    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto', includeSecondaries: true },
-    '@ngx-translate/core': { singleton: true, strictVersion: false, requiredVersion: '^14.0.0' },
+    rxjs: { requiredVersion: 'auto', includeSecondaries: true },
+    '@angular/router': { singleton: true, requiredVersion: 'auto', includeSecondaries: true },
+    '@ngx-translate/core': { singleton: true, requiredVersion: 'auto' },
     '@onecx/keycloak-auth': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/portal-integration-angular': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/accelerator': { requiredVersion: 'auto', includeSecondaries: true },
