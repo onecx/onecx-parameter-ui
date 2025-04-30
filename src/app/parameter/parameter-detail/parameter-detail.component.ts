367eb3f507e81ba472cbe5896c0844a78b9fd5cc
Henry Taeschner <github@henry-taeschner.de>
2025-04-30 07:15:02 +0200

    fix: fix code smell

--- src/app/parameter/parameter-detail/parameter-detail.component.ts
+++ src/app/parameter/parameter-detail/parameter-detail.component.ts
@@ -88,13 +88,10 @@ export function JsonValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
     if (!control.value) return null
     let isValid = true
-    let ex: unknown // sonar
     try {
       // control.value is a JavaScript object but in JSON syntax!
       JSON.parse(control.value) // is JSON?
     } catch (e) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      ex = e
       isValid = false
     }
     return isValid ? null : { pattern: true }
